c00501b541c170863606d9ac661f8ee8b92eb3da
Jonas Minnberg <sasq64@gmail.com>
2018-04-13 13:09:46 +0200

    Removed and added some files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # sixfive
-6502 assembler/emulator/disassembler
+6502 assembler/emulator/disassembler implemented in modern C++
+
+### Quickstart
+
+### Implementation Details
+
+The actual emulator is contained in a single file, `emulator.h`.
+It makes heavy use of templates, constexpr and inlining to make it
+readable and consise (very litte redundant code) while still being fast.
+
+Inlining/speed is ensured by an external test that disassembles the
+generated (x86) code for each 6502 opcode, and checks that it contains no
+calls or jumps, and that the total opcode count stays within reasonable limits
+
+For instance, here is the code generated for ORA. Most of the code is actually
+related to updating the 6502 status register.
+
+```Assembly
+    # edi points to the `Machine` object
+    movzx eax, word ptr [edi+12]      # Load PC into eax
+    lea ecx, [eax+01]                 # ecx = eax + 1 (Increment PC)
+    movzx eax, byte ptr [edi+eax+14]  # Load operand into eax
+    mov [edi+12], cx                  # Store incremented PC
+    or eax, [edi]                     # Or A into eax
+    mov [edi], eax                    # Store back A
+    # The rest of the code is just updating the status register:
+    mov ecx, FFFFFF7D
+    and ecx, [edi+0C]
+    mov edx, eax
+    and edx, 80
+    or edx, ecx
+    xor ecx, ecx
+    test eax, eax
+    sete cl
+    lea eax, [edx+ecx*2]
+    mov [edi+0C], eax
+    ret
+```
+
--- tests.cpp
+++ tests.cpp
@@ -2,205 +2,194 @@
 #include "zyan-disassembler-engine/Zydis/Zydis.hpp"
 #include <benchmark/benchmark.h>
 
-
 namespace sixfive {
 
-struct Result {
-	int calls;
-	int opcodes;
-	int jumps;
-	bool tooLong;
+struct Result
+{
+    int calls;
+    int opcodes;
+    int jumps;
+    bool tooLong;
 };
 
-void disasm(void *ptr, bool dis, struct Result &r) {
+void disasm(void* ptr, bool dis, struct Result& r)
+{
 
-	using namespace Zydis;
+    using namespace Zydis;
 
-	MemoryInput input(ptr, 0x200);
+    MemoryInput input(ptr, 0x200);
     InstructionInfo info;
     InstructionDecoder decoder;
     decoder.setDisassemblerMode(DisassemblerMode::M32BIT);
     decoder.setDataSource(&input);
     decoder.setInstructionPointer((uint64_t)ptr);
     IntelInstructionFormatter formatter;
-	r.tooLong = false;
-	r.calls = r.opcodes = r.jumps = 0;
-	while (decoder.decodeInstruction(info))
-    {
-        if(dis && !(info.flags & IF_ERROR_MASK))
-			printf("    %s\n", formatter.formatInstruction(info));
+    r.tooLong = false;
+    r.calls = r.opcodes = r.jumps = 0;
+    while (decoder.decodeInstruction(info)) {
+        if (dis && !(info.flags & IF_ERROR_MASK))
+            printf("    %s\n", formatter.formatInstruction(info));
 
-		if(info.mnemonic >= InstructionMnemonic::JA && info.mnemonic <= InstructionMnemonic::JS)
-			r.jumps++;
+        if (info.mnemonic >= InstructionMnemonic::JA &&
+            info.mnemonic <= InstructionMnemonic::JS)
+            r.jumps++;
 
-		switch(info.mnemonic) {
-		case InstructionMnemonic::RET:
-			return;
-		case InstructionMnemonic::CALL:
-			r.calls++;
-			break;
-		default:
-			break;
-		}
-		r.opcodes++;
+        switch (info.mnemonic) {
+        case InstructionMnemonic::RET: return;
+        case InstructionMnemonic::CALL: r.calls++; break;
+        default: break;
+        }
+        r.opcodes++;
     }
-	r.tooLong = true;
-	return;
-
+    r.tooLong = true;
+    return;
 }
 
-template <typename POLICY> void checkCode(bool dis) {
+template <typename POLICY> void checkCode(bool dis)
+{
 
-	using namespace sixfive;
+    using namespace sixfive;
 
-	Result r;
-	int jumps = 0;
-	int count = 0;
-	int calls = 0;
-	int opcodes = 0;
+    Result r;
+    int jumps = 0;
+    int count = 0;
+    int calls = 0;
+    int opcodes = 0;
 
-	for(const auto &i : Machine<POLICY>::getInstructions()) {
-		for(const auto &o : i.opcodes) {
+    for (const auto& i : Machine<POLICY>::getInstructions()) {
+        for (const auto& o : i.opcodes) {
             disasm((void*)o.op, dis, r);
-			printf("%s (%d/%d/%d)\n", i.name, r.opcodes, r.calls, r.jumps);
-			jumps += r.jumps;
-			calls += r.calls;
-			opcodes += r.opcodes;
-			count++;
-		}
-	}
-	printf("### AVG OPCODES: %d TOTAL OPS/CALLS/JUMPS: %d/%d/%d\n", opcodes / count, opcodes,calls, jumps);
+            printf("%s (%d/%d/%d)\n", i.name, r.opcodes, r.calls, r.jumps);
+            jumps += r.jumps;
+            calls += r.calls;
+            opcodes += r.opcodes;
+            count++;
+        }
+    }
+    printf("### AVG OPCODES: %d TOTAL OPS/CALLS/JUMPS: %d/%d/%d\n",
+           opcodes / count, opcodes, calls, jumps);
 }
 
-void checkAllCode(bool dis) {
-	checkCode<sixfive::DefaultPolicy>(dis);
+void checkAllCode(bool dis)
+{
+    checkCode<sixfive::DefaultPolicy>(dis);
 }
 
 /*
 uint32_t Machine::runDebug(uint32_t runc) {
 
-	auto toCycles = impl->cycles + runc;
-	uint32_t opcodes = 0;
-	while(impl->cycles < toCycles) {
+    auto toCycles = impl->cycles + runc;
+    uint32_t opcodes = 0;
+    while(impl->cycles < toCycles) {
 
-		checkEffect(*impl);
+        checkEffect(*impl);
 
-		uint8_t code = ReadPC(*impl);
-		printf("code %02x\n", code);
-		if(code == 0x60 && (uint8_t)impl->sp == 0xff)
-			return opcodes;
-		auto &op = jumpTable[code];
-		op.op(*impl);
-		impl->cycles += op.cycles;
-		if(impl->breaks.count(impl->pc) == 1)
-			impl->breaks[impl->pc](*this);
-		opcodes++;
-	}
-	return opcodes;
+        uint8_t code = ReadPC(*impl);
+        printf("code %02x\n", code);
+        if(code == 0x60 && (uint8_t)impl->sp == 0xff)
+            return opcodes;
+        auto &op = jumpTable[code];
+        op.op(*impl);
+        impl->cycles += op.cycles;
+        if(impl->breaks.count(impl->pc) == 1)
+            impl->breaks[impl->pc](*this);
+        opcodes++;
+    }
+    return opcodes;
 }
 */
 //} // namespace
 
+static void Bench_sort(benchmark::State& state)
+{
 
-static void Bench_sort(benchmark::State &state) {
+    static const uint8_t sortCode[] = {
+        0xa0, 0x00, 0x84, 0x32, 0xb1, 0x30, 0xaa, 0xc8, 0xca, 0xb1,
+        0x30, 0xc8, 0xd1, 0x30, 0x90, 0x10, 0xf0, 0x0e, 0x48, 0xb1,
+        0x30, 0x88, 0x91, 0x30, 0x68, 0xc8, 0x91, 0x30, 0xa9, 0xff,
+        0x85, 0x32, 0xca, 0xd0, 0xe6, 0x24, 0x32, 0x30, 0xd9, 0x60};
 
-	static const uint8_t sortCode[] =
-	{
-		0xa0, 0x00, 0x84, 0x32, 0xb1, 0x30, 0xaa, 0xc8,
-		0xca, 0xb1, 0x30, 0xc8, 0xd1, 0x30, 0x90, 0x10,
-		0xf0, 0x0e, 0x48, 0xb1, 0x30, 0x88, 0x91, 0x30,
-		0x68, 0xc8, 0x91, 0x30, 0xa9, 0xff, 0x85, 0x32,
-		0xca, 0xd0, 0xe6, 0x24, 0x32, 0x30, 0xd9, 0x60
-	};
+    static const uint8_t data[] = {
+        0,   19, 73,  2,   54,  97,  21,  45,  66,  13, 139, 56,  220, 50,
+        30,  20, 67,  111, 109, 175, 4,   66,  100, 19, 73,  2,   54,  97,
+        21,  45, 66,  13,  139, 56,  220, 50,  30,  20, 67,  111, 109, 175,
+        4,   66, 100, 19,  73,  2,   54,  97,  21,  45, 66,  13,  139, 56,
+        220, 50, 30,  20,  67,  111, 109, 175, 4,   66, 100,
+    };
 
-	static const uint8_t data[] = {
-		0,
-		19,73,2,54,97,21,45,66,13,139,56,220,50,30,20,67,111,109,175,4,66,100,
-		19,73,2,54,97,21,45,66,13,139,56,220,50,30,20,67,111,109,175,4,66,100,
-		19,73,2,54,97,21,45,66,13,139,56,220,50,30,20,67,111,109,175,4,66,100,
-	};
-
-
-	sixfive::Machine<> m;
-	for(int i=0; i<(int)sizeof(data); i++)
-		m.writeRam(0x2000 + i, data[i]);
-	for(int i=0; i<(int)sizeof(sortCode); i++)
-		m.writeRam(0x1000 + i, sortCode[i]);
-	m.writeRam(0x30, 0x00);
-	m.writeRam(0x31, 0x20);
-	m.writeRam(0x2000, sizeof(data)-1);
-	m.setPC(0x1000);
-	printf("Opcodes %d\n", m.run(50000000));
-	//uint8_t temp[256];
-	//m.readRam(0x2000, temp, sizeof(data));
-	//for(int i=0; i<sizeof(data)+1; i++)
-	//	printf("%02x ", temp[i]);
-	//puts("");
-	while(state.KeepRunning())
-	{
-		for(int i=1; i<(int)sizeof(data); i++)
-			m.writeRam(0x2000 + i, data[i]);
-		m.setPC(0x1000);
-		m.run(50000000);
-	}
-
+    sixfive::Machine<> m;
+    for (int i = 0; i < (int)sizeof(data); i++)
+        m.writeRam(0x2000 + i, data[i]);
+    for (int i = 0; i < (int)sizeof(sortCode); i++)
+        m.writeRam(0x1000 + i, sortCode[i]);
+    m.writeRam(0x30, 0x00);
+    m.writeRam(0x31, 0x20);
+    m.writeRam(0x2000, sizeof(data) - 1);
+    m.setPC(0x1000);
+    printf("Opcodes %d\n", m.run(50000000));
+    // uint8_t temp[256];
+    // m.readRam(0x2000, temp, sizeof(data));
+    // for(int i=0; i<sizeof(data)+1; i++)
+    //	printf("%02x ", temp[i]);
+    // puts("");
+    while (state.KeepRunning()) {
+        for (int i = 1; i < (int)sizeof(data); i++)
+            m.writeRam(0x2000 + i, data[i]);
+        m.setPC(0x1000);
+        m.run(50000000);
+    }
 }
 BENCHMARK(Bench_sort);
 
 struct POLICY2
 {
-	static constexpr bool Debug = true;
-	static constexpr bool AlignReads = false;
-	static constexpr bool StatusOpt = false;
-	typedef uint8_t Word;
+    static constexpr bool Debug = true;
+    static constexpr bool AlignReads = false;
+    static constexpr bool StatusOpt = false;
+    typedef uint8_t Word;
 };
 
+static void Bench_emulate(benchmark::State& state)
+{
 
-static void Bench_emulate(benchmark::State &state) {
+    static const unsigned char WEEK[] = {
+        0xa0, 0x74, 0xa2, 0x0a, 0xa9, 0x07, 0x20, 0x0a, 0x10, 0x60, 0xe0,
+        0x03, 0xb0, 0x01, 0x88, 0x49, 0x7f, 0xc0, 0xc8, 0x7d, 0x2a, 0x10,
+        0x85, 0x06, 0x98, 0x20, 0x26, 0x10, 0xe5, 0x06, 0x85, 0x06, 0x98,
+        0x4a, 0x4a, 0x18, 0x65, 0x06, 0x69, 0x07, 0x90, 0xfc, 0x60, 0x01,
+        0x05, 0x06, 0x03, 0x01, 0x05, 0x03, 0x00, 0x04, 0x02, 0x06, 0x04};
 
-	static const unsigned char WEEK[] =
-	{
-		0xa0, 0x74, 0xa2, 0x0a, 0xa9, 0x07, 0x20, 0x0a,
-		0x10, 0x60, 0xe0, 0x03, 0xb0, 0x01, 0x88, 0x49,
-		0x7f, 0xc0, 0xc8, 0x7d, 0x2a, 0x10, 0x85, 0x06,
-		0x98, 0x20, 0x26, 0x10, 0xe5, 0x06, 0x85, 0x06,
-		0x98, 0x4a, 0x4a, 0x18, 0x65, 0x06, 0x69, 0x07,
-		0x90, 0xfc, 0x60, 0x01, 0x05, 0x06, 0x03, 0x01,
-		0x05, 0x03, 0x00, 0x04, 0x02, 0x06, 0x04
-	};
-
-	sixfive::Machine<> m;
-	for(int i=0; i<(int)sizeof(WEEK); i++)
-		m.writeRam(0x1000 + i, WEEK[i]);
-	m.setPC(0x1000);
-	m.run(5000);
-	while(state.KeepRunning())
-	{
-		m.setPC(0x1000);
-		m.run(5000);
-	}
+    sixfive::Machine<> m;
+    for (int i = 0; i < (int)sizeof(WEEK); i++)
+        m.writeRam(0x1000 + i, WEEK[i]);
+    m.setPC(0x1000);
+    m.run(5000);
+    while (state.KeepRunning()) {
+        m.setPC(0x1000);
+        m.run(5000);
+    }
 };
 
 BENCHMARK(Bench_emulate);
 
-static void Bench_allops(benchmark::State &state) {
-	sixfive::Machine<> m;
-	m.setPC(0x1000);
-	while(state.KeepRunning()) {
-		m.setPC(0x1000);
-		for(const auto &i : m.getInstructions()) {
-			for(const auto &o : i.opcodes) {
-				o.op(m);
-			}
-		}
-	}
-
+static void Bench_allops(benchmark::State& state)
+{
+    sixfive::Machine<> m;
+    m.setPC(0x1000);
+    while (state.KeepRunning()) {
+        m.setPC(0x1000);
+        for (const auto& i : m.getInstructions()) {
+            for (const auto& o : i.opcodes) {
+                o.op(m);
+            }
+        }
+    }
 }
 BENCHMARK(Bench_allops);
 /*
 int main(int argc, char **argv) {
-	sixfive::Machine<> m;
-	return 0;
+    sixfive::Machine<> m;
+    return 0;
 }*/
 
-} 
+} // namespace sixfive
